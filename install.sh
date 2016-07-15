4200e9ecd96935a78aa087aec243f76c2294b795
Michael SanAngelo <msanangelo@gmail.com>
2016-07-15 01:52:39 -0500

    fixed echo lines in upload script

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-echo -n "Install Bash Profile";
+echo "Install Bash Profile";
 rm ~/.bash_profile .profile;
 ln -s ~/.dotfiles/bash/bash_profile ~/.bash_profile;
 ln -s ~/.dotfiles/bash/profile ~/.profile;
 source ~/.bash_profile;
-echo -n "Install Bin Files";
+echo "Install Bin Files";
 mkdir ~/bin;
 ln -s ~/.dotfiles/bin/upload.sh ~/bin/upload.sh;
 exit 0;
