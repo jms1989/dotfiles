d13ba1f0ffa847877e3fd5dfab1d60861469f6b6
Michael SanAngelo <msanangelo@gmail.com>
2016-07-15 01:51:04 -0500

    update install script

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+echo -n "Install Bash Profile";
 rm ~/.bash_profile .profile;
 ln -s ~/.dotfiles/bash/bash_profile ~/.bash_profile;
-ln -s ~/.dotfiles/bash/profiles ~/.profiles;
+ln -s ~/.dotfiles/bash/profile ~/.profile;
+source ~/.bash_profile;
+echo -n "Install Bin Files";
+mkdir ~/bin;
+ln -s ~/.dotfiles/bin/upload.sh ~/bin/upload.sh;
+exit 0;
