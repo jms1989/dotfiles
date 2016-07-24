c3639fa4a914b3e9743fe7e2372d808c5352b95c
Michael SanAngelo <msanangelo@gmail.com>
2016-07-24 02:58:20 -0500

    add past tense to echos

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo "Install Bash Profile.";
+echo "Installed Bash Profile.";
 rm ~/.bash_profile ~/.profile ~/.bashrc 2> /dev/null;
 ln -s ~/.dotfiles/bash/bash_profile ~/.bash_profile;
 ln -s ~/.dotfiles/bash/profile ~/.profile;
 ln -s ~/.dotfiles/bash/bashrc ~/.bashrc;
 touch ~/.environment;
-echo "Install Bin Files.";
+echo "Installed Bin Files.";
 mkdir ~/bin;
 chmod +x ~/.dotfiles/bin/*.sh;
 ln -s ~/.dotfiles/bin/upload.sh ~/bin/upload.sh;
