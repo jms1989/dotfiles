ddb423dde211f29bfe51e8d2d2d369469fd4d228
Michael SanAngelo <msanangelo@gmail.com>
2016-07-23 22:01:43 -0500

    supress rm errr in installer

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Install Bash Profile";
-rm ~/.bash_profile .profile;
+rm ~/.bash_profile ~/.profile 2> /dev/null;
 ln -s ~/.dotfiles/bash/bash_profile ~/.bash_profile;
 ln -s ~/.dotfiles/bash/profile ~/.profile;
 source ~/.bash_profile;
