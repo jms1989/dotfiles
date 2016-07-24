f0ab2fcbf82f1b209b737e4bf6370e42c18934c4
Michael SanAngelo <msanangelo@gmail.com>
2016-07-23 22:16:37 -0500

    fix nginx file again and add index file

--- bin/setup-nginx.sh
+++ bin/setup-nginx.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 rm /etc/nginx/sites-available/default;
-ln -s /home/micahel/.dotfiles/nginx/default /etc/nginx/sites-available/default;
+ln -s /home/michael/.dotfiles/nginx/default /etc/nginx/sites-available/default;
 service nginx reload;
+cp /home/michael/.dotfiles/nginx/index.html /home/michael/workspace/index.html;
+chown michael: /home/michael/workspace/index.html;
 exit
