3c01a54e00430aa29f51cde3c0998b726b97df4d
Michael SanAngelo <msanangelo@gmail.com>
2016-07-23 22:09:46 -0500

    fix setup-nginx file

--- bin/setup-nginx.sh
+++ bin/setup-nginx.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rm /etc/nginx/sites-available/default;
-ln -s ~/.dotfiles/nginx/default /etc/nginx/sites-available/default;
+ln -s /home/micahel/.dotfiles/nginx/default /etc/nginx/sites-available/default;
 service nginx reload;
 exit
