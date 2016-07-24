2155d06e9644bb5c38a54e5ec29da4f02b7b9d6c
Michael SanAngelo <msanangelo@gmail.com>
2016-07-24 00:07:22 -0500

    fix file delete

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Install Bash Profile";
-rm ~/.bash_profile ~/.profile 2> /dev/null;
+rm ~/.bash_profile ~/.profile ~/.bashrc 2> /dev/null;
 ln -s ~/.dotfiles/bash/bash_profile ~/.bash_profile;
 ln -s ~/.dotfiles/bash/profile ~/.profile;
 ln -s ~/.dotfiles/bash/bashrc ~/.bashrc;
