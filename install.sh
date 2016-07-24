beb9a83ebdf006581e0f47336b299ca6fb5ef1e3
Michael SanAngelo <msanangelo@gmail.com>
2016-07-24 00:24:51 -0500

    change some echo lines

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-echo "Install Bash Profile";
+echo "Install Bash Profile.";
 rm ~/.bash_profile ~/.profile ~/.bashrc 2> /dev/null;
 ln -s ~/.dotfiles/bash/bash_profile ~/.bash_profile;
 ln -s ~/.dotfiles/bash/profile ~/.profile;
 ln -s ~/.dotfiles/bash/bashrc ~/.bashrc;
 touch ~/.environment;
-echo "Install Bin Files";
+echo "Install Bin Files.";
 mkdir ~/bin;
 chmod +x ~/.dotfiles/bin/*.sh;
 ln -s ~/.dotfiles/bin/upload.sh ~/bin/upload.sh;
-echo "Please logout and log back in to apply environmental scripts or source .bashrc and .bash_profile";
+echo "Please logout and log back in to apply environmental scripts or source .bash_profile.";
 exit 0;
