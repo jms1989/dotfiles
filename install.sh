b354c219f0d5ba65ad955b9123292e10dde84e90
Michael SanAngelo <jms1989@users.noreply.github.com>
2016-11-28 06:54:03 -0600

    add screenrc to installer

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ rm ~/.bash_profile ~/.profile ~/.bashrc 2> /dev/null;
 ln -s ~/.dotfiles/bash/bash_profile ~/.bash_profile;
 ln -s ~/.dotfiles/bash/profile ~/.profile;
 ln -s ~/.dotfiles/bash/bashrc ~/.bashrc;
+ln -s ~/.dotfiles/bash/screenrc ~/.screenrc;
 touch ~/.environment;
 echo "Installed Bin Files.";
 mkdir ~/bin;
