0f33527e5cf68db106ef0823aa126cb2849ba25c
Michael SanAngelo <msanangelo@gmail.com>
2017-03-01 17:41:33 -0600

    fix the script again

--- bin/setup-apache2.sh
+++ bin/setup-apache2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Setting up apache configs";
-sudo rm /etc/apache2/apache2.conf /etc/apache2/sites/available/000-default.conf;
+sudo rm /etc/apache2/apache2.conf /etc/apache2/sites-available/000-default.conf;
 sudo ln -s ~/.dotfiles/conf/apache2/apache2.conf /etc/apache2/apache2.conf;
 sudo ln -s ~/.dotfiles/conf/apache2/sites-available/000-default.conf /etc/apache2/sites-available/000-default.conf;
 mkdir ~/workspace;
