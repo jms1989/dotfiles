669b4fcf0ac28baccff6ff78e5efc9e23ea502d4
Michael SanAngelo <msanangelo@gmail.com>
2017-03-01 20:11:37 -0600

    add mod enable rewrite for apache

--- bin/setup-apache2.sh
+++ bin/setup-apache2.sh
@@ -4,6 +4,7 @@ sudo rm /etc/apache2/apache2.conf /etc/apache2/sites-available/000-default.conf;
 sudo ln -s ~/.dotfiles/conf/apache2/apache2.conf /etc/apache2/apache2.conf;
 sudo ln -s ~/.dotfiles/conf/apache2/sites-available/000-default.conf /etc/apache2/sites-available/000-default.conf;
 mkdir ~/workspace;
+sudo a2enmod rewrite;
 sudo service apache2 restart;
 exit
 
