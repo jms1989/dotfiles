2424aceb4ebacd9dc5b3d90c6df4875f5bfb5686
Michael SanAngelo <msanangelo@gmail.com>
2018-09-10 06:16:02 +0000

    update nodejs setup script to not verify ssl

--- bin/setup-nodejs.sh
+++ bin/setup-nodejs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Setting up NodeJS...";
-curl -sL https://deb.nodesource.com/setup_8.x -o nodesource_setup.sh > /dev/null;
-sudo bash nodesource_setup.sh > /dev/null;
-sudo apt update > /dev/null;
-sudo apt install -y nodejs > /dev/null;
+curl -sLK https://deb.nodesource.com/setup_8.x -o /tmp/nodesource_setup.sh > /dev/null;
+sudo bash /tmp/nodesource_setup.sh > /dev/null;
+sudo apt-get update > /dev/null;
+sudo apt-get install -y nodejs > /dev/null;
 exit
