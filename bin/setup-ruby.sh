bec416ce6bab0f8036629242e86b1e6a4e3f07d2
Michael SanAngelo <msanangelo@gmail.com>
2018-09-10 12:54:12 -0500

    add ruby setup

---
+++ bin/setup-ruby.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Setting up Ruby...";
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB > /dev/null
+curl -sSL https://get.rvm.io | bash -s stable --ruby > /dev/null
+source /home/michael/.rvm/scripts/rvm
+#echo "Current RVM Version $('rvm current')";
+#echo "Current RVM Version $('ruby -v')";
+exit
