cbe21f9e9763bc7e6bb8e56480b44e956a697acb
Michael SanAngelo <msanangelo@gmail.com>
2018-09-10 12:53:38 -0500

    fix nodejs curl line

--- bin/setup-nodejs.sh
+++ bin/setup-nodejs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Setting up NodeJS...";
-curl -sLK https://deb.nodesource.com/setup_8.x -o /tmp/nodesource_setup.sh > /dev/null;
+curl -sL https://deb.nodesource.com/setup_8.x -o /tmp/nodesource_setup.sh > /dev/null;
 sudo bash /tmp/nodesource_setup.sh > /dev/null;
 sudo apt-get update > /dev/null;
 sudo apt-get install -y nodejs > /dev/null;
